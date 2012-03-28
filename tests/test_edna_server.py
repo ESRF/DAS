755fd1d5747ab61614596c8504279b7aaea3a780
Olof Svensson <svensson@esrf.fr>
2012-03-28 22:56:29 +0200

    Removed the class DASStateMachine, improved the test script

--- tests/test_edna_server.py
+++ tests/test_edna_server.py
@@ -5,40 +5,73 @@ if len(sys.argv) < 2:
     print "Usage: %s edna-tango-device-name" % os.path.basename(sys.argv[0])
     sys.exit(1)
 
-def jobFinished(event):
-    print "Job finished!", event.attr_value.value
-    if event.attr_value.value is not None:
-        jobFinishedId, status = event.attr_value.value
-        if status == "success":
-            print dev.getJobOutput(jobFinishedId)
 
-def success(event):
-    print "SUCCESS!", event.attr_value.value
-    if event.attr_value.value is not None:
-        print dev.getJobOutput(event.attr_value.value)
+class TestDasEdnaServer(object):
 
-def failure(event):
-    print "FAILURE!", event.attr_value.value
+    def setUp(self):
+        self.dev = PyTango.DeviceProxy(sys.argv[1])
+        self.dev.subscribe_event("jobSuccess", PyTango.EventType.CHANGE_EVENT, self.successCallback, [])
+        self.dev.subscribe_event("jobFailure", PyTango.EventType.CHANGE_EVENT, self.failureCallback, [])
+        self.success = None
+        self.failure = None
+        self.jobOutput = None
+
+    def successCallback(self, event):
+        self.success = event.attr_value.value
+        #print "SUCCESS!", event.attr_value.value
+        if event.attr_value.value is not None:
+            self.jobOutput = self.dev.getJobOutput(event.attr_value.value)
+    
+    def failureCallback(self, event):
+        self.failure = event.attr_value.value
+        #print "FAILURE!", event.attr_value.value
 
 
-dev = PyTango.DeviceProxy(sys.argv[1])
-dev.subscribe_event("jobFinished", PyTango.EventType.CHANGE_EVENT, jobFinished, [])
-dev.subscribe_event("jobSuccess", PyTango.EventType.CHANGE_EVENT, success, [])
-dev.subscribe_event("jobFailure", PyTango.EventType.CHANGE_EVENT, failure, [])
+    def test_EDPluginTestPluginFactory(self):
+        
+        print "------------------------------------------------------------------------"
 
-print "------------------------------------------------------------------------"
+        strXML = """<?xml version="1.0" ?>
+        <XSDataString>
+            <value>Test string value.</value>
+        </XSDataString>"""
 
-strXML = """<?xml version="1.0" ?>
-<XSDataString>
-    <value>Test string value.</value>
-</XSDataString>"""
+        strEdnaPlugin = "EDPluginTestPluginFactory"
 
-#strEdnaPlugin = "EDPluginControlInterfaceToMXCuBEv1_3"
-strEdnaPlugin = "EDPluginTestPluginFactory1"
-try:
-  jobId = dev.command_inout("startJob", [strEdnaPlugin, strXML])
-  print "Started job: ", jobId
-except Exception, e:
-    print "Error! ", e
+        jobId = self.dev.command_inout("startJob", [strEdnaPlugin, strXML])
+        print "Started job: ", jobId
 
-time.sleep(1)
+        # Wait for callback...
+        bContinue = True
+        fTotalTime = 0.0
+        fMaxTime = 10.0
+        while bContinue:
+            print "Waiting for callback..."
+            if self.success == jobId:
+                #print self.jobOutput
+                bContinue = False
+            elif self.failure == jobId:
+                #print "FAILURE!"
+                bContinue = False
+            else:
+                time.sleep(1)
+                fTotalTime += 1.0
+                if fTotalTime > fMaxTime:
+                    bContinue = False
+        
+        if self.jobOutput.find("Test string value.") != -1:
+            print "Test succeeded!"
+            sys.exit(0)
+        else:
+            print "Test failed..."
+            sys.exit(1)
+
+if __name__ == '__main__':
+    testDasEdnaServer = TestDasEdnaServer()
+    testDasEdnaServer.setUp()
+    testDasEdnaServer.test_EDPluginTestPluginFactory()
+
+
+
+
+
