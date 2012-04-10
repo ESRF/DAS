947779ade00d75f8fd4021d92fc89f166e6c7a73
Olof Svensson <svensson@esrf.fr>
2012-04-10 22:11:27 +0200

    Modified to launch many tests

--- tests/test_edna_mx.py
+++ tests/test_edna_mx.py
@@ -1,35 +1,42 @@
 import PyTango
-import time, sys, os
+import time, sys, os, threading
 
 if len(sys.argv) < 2:
     print "Usage: %s edna-tango-device-name" % os.path.basename(sys.argv[0])
     sys.exit(1)
 
 
-class TestDasEdnaServer(object):
+class TestDasEdnaServer(threading.Thread):
 
-    def setUp(self):
-        self.dev = PyTango.DeviceProxy(sys.argv[1])
-        self.dev.subscribe_event("jobSuccess", PyTango.EventType.CHANGE_EVENT, self.successCallback, [])
-        self.dev.subscribe_event("jobFailure", PyTango.EventType.CHANGE_EVENT, self.failureCallback, [])
-        self.success = None
-        self.failure = None
-        self.jobOutput = None
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self._fTimeOut = 100.0 # s
+        self._dev = PyTango.DeviceProxy(sys.argv[1])
+        self._dev.subscribe_event("jobSuccess", PyTango.EventType.CHANGE_EVENT, self.successCallback, [])
+        self._dev.subscribe_event("jobFailure", PyTango.EventType.CHANGE_EVENT, self.failureCallback, [])
+        self._success = None
+        self._failure = None
+        self._jobOutput = None
+        self._fExecutionTime = 0.0
+        self._strJobId = None
+        self._bSuccess = None
 
     def successCallback(self, event):
-        self.success = event.attr_value.value
-        #print "SUCCESS!", event.attr_value.value
-        if event.attr_value.value is not None:
-            self.jobOutput = self.dev.getJobOutput(event.attr_value.value)
+        if event.attr_value is not None:
+            self._success = event.attr_value.value
+            #print "SUCCESS!", event.attr_value.value
+            if event.attr_value.value is not None:
+                self._jobOutput = self._dev.getJobOutput(event.attr_value.value)
     
     def failureCallback(self, event):
-        self.failure = event.attr_value.value
+        if event.attr_value is not None:
+            self._failure = event.attr_value.value
         #print "FAILURE!", event.attr_value.value
 
 
-    def test_EDPluginControlInterfaceToMXCuBEv1_3(self):
+    def run(self):
         
-        print "------------------------------------------------------------------------"
+        #print "------------------------------------------------------------------------"
 
         strXML = """<?xml version="1.0" ?>
 <XSDataInputMXCuBE>
@@ -125,40 +132,67 @@ class TestDasEdnaServer(object):
 
         strEdnaPlugin = "EDPluginControlInterfaceToMXCuBEv1_3"
 
-        jobId = self.dev.command_inout("startJob", [strEdnaPlugin, strXML])
-        print "Started job: ", jobId
+        self._strJobId = self._dev.command_inout("startJob", [strEdnaPlugin, strXML])
+        #print "Started job: ", self._strJobId
 
         # Wait for callback...
         bContinue = True
-        fTotalTime = 0.0
-        fMaxTime = 300.0
+        self._fExecutionTime = 0.0
         while bContinue:
-            print "JobID %s : Waiting for callback, time %f" % (jobId, fTotalTime)
-            if self.success == jobId:
+            #print "self._strJobId %s : Waiting for callback, time %f" % (self._strJobId, fExecutionTime)
+            if self._success == self._strJobId:
                 #print self.jobOutput
                 bContinue = False
-            elif self.failure == jobId:
+            elif self._failure == self._strJobId:
                 #print "FAILURE!"
                 bContinue = False
             else:
                 time.sleep(1)
-                fTotalTime += 1.0
-                if fTotalTime > fMaxTime:
+                self._fExecutionTime += 1.0
+                if self._fExecutionTime > self._fTimeOut:
                     bContinue = False
         
-        if self.jobOutput is not None:
+        if self._jobOutput is not None:
             #print self.jobOutput
-            if self.jobOutput.find("CharacterisationResult.xml") != -1:
-                print "Test succeeded!"
-                sys.exit(0)
+            if self._jobOutput.find("CharacterisationResult.xml") != -1:
+                self._bSuccess = True
             else:
-                print "Test failed..."
-                sys.exit(1)
+                self._bSuccess = False
+                
+                
+    def isSuccess(self):
+        return self._bSuccess
+                
+    def getJobId(self):
+        return self._strJobId
+    
+    def getExecutionTime(self):
+        return self._fExecutionTime
+    
+    def setTimeOut(self, _fValue):
+        self._fTimeOut = _fValue
 
 if __name__ == '__main__':
-    testDasEdnaServer = TestDasEdnaServer()
-    testDasEdnaServer.setUp()
-    testDasEdnaServer.test_EDPluginControlInterfaceToMXCuBEv1_3()
+    for i in range(5):
+        listTestDasEdnaServer = []
+        # Start the servers
+        for j in range(1):
+            testDasEdnaServer = TestDasEdnaServer()
+            testDasEdnaServer.start()
+            listTestDasEdnaServer.append(testDasEdnaServer)
+            time.sleep(1)
+        for testDasEdnaServer in listTestDasEdnaServer:
+            while testDasEdnaServer.getJobId() is None:
+                time.sleep(1)
+            print("%s started!" % testDasEdnaServer.getJobId())
+        # Synchronize servers
+        for testDasEdnaServer in listTestDasEdnaServer:
+            testDasEdnaServer.join()
+            if testDasEdnaServer.isSuccess():
+                print("%s success! %.1f s" % (testDasEdnaServer.getJobId(), testDasEdnaServer.getExecutionTime()))
+            else:
+                print("%s failure! %.1f s" % (testDasEdnaServer.getJobId(), testDasEdnaServer.getExecutionTime()))
+        time.sleep(1)
 
 
 
